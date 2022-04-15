06dad91f4d4db1e8aa5dfd02e8f76f8e4556c60b
Shivani Sivakumar <shivani@Shivanis-MacBook-Air.local>
2022-04-15 17:16:14 +0200

    html project

---
+++ Udemy coding.js
@@ -0,0 +1,79 @@
+var coding = "coding";
+var Fun = "Fun";
+console.write(Fun + coding); // Fun coding
+
+stirnginput;// User can say anything so we keep it empty.
+console.ReadLine(input);// First Read the input.  
+console.WriteLine(input);// Second give the output.
+
+location = new Vector2(15,25); // y Coordinates move up and Down x Coordinates move Left and Right.
+AppObject.Vector2 = location;
+
+
+If("You dont Water the plant the plant will died.")
+{}
+
+if (Youtubeisopen == true)
+{
+    // Play some learning vidos
+}
+else{
+    // Play some relaxing songs
+}
+
+
+if (variable <= 5 )
+{
+    //say you are smaller
+}
+else if (variable >= 6 )
+{
+      //say you are greater
+}
+
+
+if (variable == 8)
+{
+    // Some code
+}
+else if (variable >= 8)
+{
+    // Some code
+}
+
+
+if (variable != 8)
+{
+    // Some code
+}
+else if (variable == 8)
+{
+    // Some code
+}
+
+
+if ((variable <=8) && (boolean = true))
+{
+  // Some code  
+}
+else {
+    // Some code
+}
+
+
+
+if ((int == 8)||(boolean == true))
+{
+    // Some code
+}
+else ((int == 7)||(boolean == true))
+{
+    // Some code
+}
+
+
+
+//if ((int == 0) && (boolean == false) && (boolean1 == true) && (int == 5) && (stirng == "plant") && (boolean2 == false) && (int == 7) && (stirng ==" Aby party") && (int == 22) && (boolean == true)
+{
+  // Some code  
+}
